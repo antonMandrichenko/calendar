321a96d054eb5497b944dcc1e69aaf2a294e2792
Anton Mandrichenko <arnellomutty2@gmail.com>
2019-03-04 10:35:14 +0200

    Updates

---
+++ precache-manifest.1169c6dd494627b29a266271ed974512.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bcf0e1999ccf1fed4e8",
+    "url": "/calendar/static/js/runtime~main.8bcf0e19.js"
+  },
+  {
+    "revision": "38539c1283ca87c5e1a4",
+    "url": "/calendar/static/js/main.38539c12.chunk.js"
+  },
+  {
+    "revision": "f7195570aa49c17a4cf3",
+    "url": "/calendar/static/js/2.f7195570.chunk.js"
+  },
+  {
+    "revision": "38539c1283ca87c5e1a4",
+    "url": "/calendar/static/css/main.1fe5f9b9.chunk.css"
+  },
+  {
+    "revision": "5741c1ac87c27a51d4d9bac4012621b6",
+    "url": "/calendar/index.html"
+  }
+];
